293cff3705297e6cfbadb8b15280ff82d7e5e1f3
origamiwang <2241375541@qq.com>
2023-05-06 10:31:25 +0800

    add pprint test

--- ModulePathDemo/main.py
+++ ModulePathDemo/main.py
@@ -1,13 +1,25 @@
 import test1
 import sys
+
 # from test_module.test2 import test2
 sys.path.append('test_module')
 import test2
 
 import pprint
 
+
+def test_pprint():
+    kudamono_dict = {'apple': {'juice': {1: 2, 3: 4, 5: 6},
+                               'pie': {1: 3, 2: 4, 5: 7}},
+                     'orange': {'cake': {3: 2, 5: 4, 6: 5},
+                                'juice': {1: 5, 2: 3, 5: 6}},
+                     'pear': {'cake': {1: 6, 6: 1, 7: 8},
+                              'pie': {3: 5, 5: 3, 8: 7}}}
+    pprint.pprint(kudamono_dict, width=30, indent=10)
+
+
 if __name__ == '__main__':
-    # examine the system path you had added
-    pprint.pprint(sys.path)
-    test1.test1()
-    test2.test2()
+    test_pprint()
+# examine the system path you had added
+# test1.test1()
+# test2.test2()
